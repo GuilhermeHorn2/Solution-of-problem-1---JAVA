49a08f0b321d35c246056fab284131b27505e6a1
GuilhermeHorn2 <74984552+GuilhermeHorn2@users.noreply.github.com>
2023-03-18 21:21:58 -0300

    Create Optmized.java

---
+++ Optmized.java
@@ -0,0 +1,44 @@
+package misc;
+
+public class main_misc {
+
+	public static void main(String[] args) {
+		//
+	int[] v = {0,1,0,0,1};
+	System.out.println(bulb(v));
+		
+	}
+	public static int bulb(int[]v) {
+		//
+		int c = 0;
+		for(int i = 0;i < v.length;i++) {
+			//check if the number of flips is even
+			//if is even we dont flip
+			if(c % 2 == 0) {
+				v[i] = v[i];
+			}
+			//if is not even we flip
+			else if(c % 2 != 0){
+				if(v[i] == 1) {
+					v[i] = 0;
+				}
+				else if(v[i] == 0) {
+					v[i] = 1;
+				}
+			}
+			//if v[i] is 1 we dont need do do nothing
+			if(v[i] == 1) {
+				continue;
+			}
+			//if v[i] == 1 the bit was fliped
+			else {
+				c++;
+			}
+			
+			
+		}
+		return c;
+		
+	}
+
+}
